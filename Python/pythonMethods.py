b6442cb60e904ebecb4535547a3090b39f868bea
Shahil Shaikh <shaikhsahil4495@gmail.com>
2023-12-28 13:41:35 +0530

    Day 3

--- Python/pythonMethods.py
+++ Python/pythonMethods.py
@@ -41,4 +41,18 @@ a=int(input())
 for i in range(1,11):
     print(f"{i}*{a}=",i*a, end=" ")
 ======================================================================
+# len(), max() and min() - here max and min will return the element based on the ascii values
+a=input().split("1")
+print(a)
+print(len(max(a)))
+======================================================================
+# create hollow box using hash
+n=int(input())
+for i in range(n):
+    if (i==0 or i==n-1):
+        print(n*"# ")
+    else:
+        print("# "+"  "*(n-2)+" #")
+======================================================================
 """
+
