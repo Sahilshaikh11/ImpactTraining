47748808d6bf917c2589aa7ca140f028ecc4c448
Shahil Shaikh <shaikhsahil4495@gmail.com>
2024-01-25 14:23:40 +0530

    Day 21

---
+++ DataStructures/Parul_array.py
@@ -0,0 +1,113 @@
+'''
+
+# Q1. subarray sum equals k
+Given an array of integers nums and an integer k, 
+return the total number of subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1], k = 2
+Output: 2
+Example 2:
+
+Input: nums = [1,2,3], k = 3
+Output: 2
+
+
+-----------------------------------------------------------------------------------------------------------------
+
+
+# maximum summation 
+Given an integer array nums, find the 
+subarray
+ with the largest sum, and return its sum.
+
+ 
+
+Example 1:
+
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+Example 2:
+
+Input: nums = [1]
+Output: 1
+Explanation: The subarray [1] has the largest sum 1.
+Example 3:
+
+Input: nums = [5,4,-1,7,8]
+Output: 23
+Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
+
+-----------------------------------------------------------------------------------------------------
+
+
+# neighbours not equal k
+You are given a 0-indexed array nums of distinct integers. 
+You want to rearrange the elements in the array such that every element
+in the rearranged array is not equal to the average of its neighbors.
+
+More formally, the rearranged array should have the property such that for 
+every i in the range 1 <= i < nums.length - 1, (nums[i-1] + nums[i+1]) / 2 is not 
+equal to nums[i].
+
+Return any rearrangement of nums that meets the requirements.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,4,5]
+Output: [1,2,4,5,3]
+Explanation:
+When i=1, nums[i] = 2, and the average of its neighbors is (1+4) / 2 = 2.5.
+When i=2, nums[i] = 4, and the average of its neighbors is (2+5) / 2 = 3.5.
+When i=3, nums[i] = 5, and the average of its neighbors is (4+3) / 2 = 3.5.
+Example 2:
+
+Input: nums = [6,2,0,9,7]
+Output: [9,7,6,2,0]
+Explanation:
+When i=1, nums[i] = 7, and the average of its neighbors is (9+6) / 2 = 7.5.
+When i=2, nums[i] = 6, and the average of its neighbors is (7+2) / 2 = 4.5.
+When i=3, nums[i] = 2, and the average of its neighbors is (6+0) / 2 = 3.
+
+
+-------------------------------------------------------------------------------------------------------------------
+
+
+# next permutation 
+A permutation of an array of integers is an arrangement of its members 
+into a sequence or linear order.
+
+For example, for arr = [1,2,3], the following are all the permutations of
+arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+The next permutation of an array of integers is the next lexicographically
+greater permutation of its integer. More formally, if all the permutations 
+of the array are sorted in one container according to their lexicographical order, 
+then the next permutation of that array is the permutation that follows it in the
+sorted container. If such arrangement is not possible, the array must be rearranged 
+as the lowest possible order (i.e., sorted in ascending order).
+
+For example, the next permutation of arr = [1,2,3] is [1,3,2].
+Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does 
+not have a lexicographical larger rearrangement.
+Given an array of integers nums, find the next permutation of nums.
+
+The replacement must be in place and use only constant extra memory.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [1,3,2]
+
+'''
